039305ba6c34f7445d6d3691d9080b1a338f8376
Alfonso Lambiase <lambiase1199@gmail.com>
2025-04-09 18:06:41 +0200

    add ex5 commenti

--- 5es_State/EsercizioReact/src/App.jsx
+++ 5es_State/EsercizioReact/src/App.jsx
@@ -9,3 +9,13 @@ const App = () => {
 }
 
 export default App;
+
+
+
+/*
+Quando si chiama la funzione "setter" per incrementare il contatore, il parametro dovrebbe essere una funzione
+o un valore immediato? Perché?
+
+-USARE UNA FUNZIONE COME PARAMETRO E' IL METODO MGLIORE PER L'AGGIORNAMENTO.
+USARE UN VALORE IMMEDIATO PUO' CAUSARE PROBLEMI SE CI SONO PIU' AGGIORNAMENTI SIMULTANEI O SE VIENE FATTO IN MODO ASINCRONO.
+*/
