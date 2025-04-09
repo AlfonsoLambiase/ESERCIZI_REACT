49ee697af747afe108a5bb940d7f3550e850a068
Alfonso Lambiase <lambiase1199@gmail.com>
2025-04-09 18:50:37 +0200

    add ex css

--- 4es_Event/EsercizioReact/src/AlertClock.jsx
+++ 4es_Event/EsercizioReact/src/AlertClock.jsx
@@ -1,3 +1,4 @@
+import "./AlertClock.css"
 
 const AlertClock = ({Time}) => {                       
   return <button onClick={Time}>Ora Esatta!</button>;    // AGGIUNGO AL BOTTONE L'EVENTO ONCLICK PASSANDO LA PROPS DESTRUTTURATA TIME
