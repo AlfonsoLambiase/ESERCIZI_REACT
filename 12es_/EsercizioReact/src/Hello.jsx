7be3132d4488c70146b0a748c80e339ebac0fd93
Alfonso Lambiase <lambiase1199@gmail.com>
2025-04-15 13:39:12 +0200

    add ex10

---
+++ 12es_/EsercizioReact/src/Hello.jsx
@@ -0,0 +1,12 @@
+import Messaggio from "./Messaggio";
+
+function Hello(){
+    return(
+        <>
+        <h2>Hello World</h2>
+        <Messaggio/>
+        </>
+    )
+}
+
+export default Hello;
