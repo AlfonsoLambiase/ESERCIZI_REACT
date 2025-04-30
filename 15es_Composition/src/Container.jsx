4df0cf3c26ca170410e49f0064e0ae59354e022f
Alfonso Lambiase <lambiase1199@gmail.com>
2025-04-30 07:09:05 +0200

    add ex16

--- 15es_Composition/src/Container.jsx
+++ 15es_Composition/src/Container.jsx
@@ -1,19 +1,33 @@
+/*
+-Crea un componente Container  che esegua il rendering dei suoi elementi figlio all'interno di un div.
+-Usate un div con uno sfondo bianco e un bordo rosso.
+-Aggiungere un title prop che conterrà il valore del titolo da visualizzare prima degli 
+elementi figlio.
+-Rendi comprimibile il contenitore, in modo che quando si clicca sul titolo gli elementi 
+secondari vengano nascosti o nuovamente mostrati.
+-Utilizzare il useState per tenere traccia dello stato compresso.
+*/
+
 import { useState } from "react";
 
-const Container = ({children, title})=>{
+const Container = ({ children, title }) => {
+  const [visibile, setVisibile] = useState(false);
 
-    const [visibile, setVisibile] = useState(false)
+  const handleClick = () => {
+    setVisibile(!visibile);
+  };
 
-const handleClick = ()=>{
-setVisibile(!visibile)
-}
-
-    return(
-       <div style={{ backgroundColor: "white", border: "1px solid red", width:"fit-content" }}>
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        border: "1px solid red",
+        width: "fit-content",
+      }}>
       <h1 onClick={handleClick}>{title}</h1>
       {visibile && children}
-       </div>
-    )
+    </div>
+  );
 };
 
 export default Container;
